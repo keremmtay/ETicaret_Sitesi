41520f7ce8f24ccf3a51f3cbcae290cb26e027f6
Mustafa Kerem Tay <81324994+keremmtay@users.noreply.github.com>
2023-06-20 14:50:55 +0300

    Add project files.

---
+++ ETicaret.WebUI/Controllers/OrderController.cs
@@ -0,0 +1,31 @@
+﻿using ETicaret.BusinessLayer.Abstract;
+using ETicaret.WebUI.Identity;
+using ETicaret.WebUI.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaret.WebUI.Controllers
+{
+    [Authorize(Roles ="Customer")]
+    public class OrderController : Controller
+    {
+        private UserManager<User> _userManager;
+        private IOrderService _orderService;
+
+        public OrderController(UserManager<User> userManager, IOrderService orderService)
+        {
+            _userManager = userManager;
+            _orderService = orderService;
+        }
+
+        public IActionResult List()
+        {
+            var userId = _userManager.GetUserId(User);
+
+            var orders = _orderService.GetOrders(userId);
+
+            return View(orders);
+        }
+    }
+}
